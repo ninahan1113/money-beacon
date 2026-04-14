95467a0a3a97390945f57b3932d37af37b0bea98
ninahan1113 <alinahan1113@gmail.com>
2026-04-14 15:18:56 +0800

    Deploy Money Beacon v1

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'money-beacon-v1'
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll(['/money-beacon/'])
+    )
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
